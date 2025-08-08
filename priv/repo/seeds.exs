bb62582cb57396fe3be67aefd4da74daf68bc4d7
Andrii Bohdan <bohdan.andrii@gmail.com>
2025-08-07 21:09:10 -0400

    Seed default list and item (#9)
    * chore(assets): remove leftover delete.svg from assets/static
    
    No longer used after switching to heroicons.
    
    * feat(seed): create default list and item when empty
    
    If there are no lists, create a default list and a default item ("Conquer the world"). Idempotent and safe to re-run.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,19 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     ElixirTodo.Repo.insert!(%ElixirTodo.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias ElixirTodo.Repo
+alias ElixirTodo.Todos
+alias ElixirTodo.Todos.List
+
+# Create a default list and item if there are no lists yet.
+case Repo.aggregate(List, :count, :id) do
+  0 ->
+    {:ok, list} =
+      Todos.create_list(%{
+        name: "Default",
+        color: "#22c55e" # green-500
+      })
+
+    _ = Todos.create_item(%{text: "Conquer the world", completed: false, list_id: list.id})
+    :ok
+
+  _ ->
+    :ok
+end
